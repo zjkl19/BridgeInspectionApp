c25d47585037f2ad293682e5d5e7b8b4293509c9
迪南 林 <eamdfan@hotmail.com>
2024-05-09 22:13:25 +0800

    涉及到在 MainPage.xaml 文件中添加一个新按钮和四个标签，以及在 MainPage.xaml.cs 文件中添加一个新方法的最重要的变化。 1. 在 MainPage.xaml 文件中添加了一个标签为 "注意事项" 的新按钮。此按钮被设计为在被点击时触发 "OnNoticeClicked" 事件。它位于页面的中心，垂直和水平都居中，并且具有蓝色的背景。 2. 在 MainPage.xaml 文件中添加了四个新的标签。这些标签用于显示版权信息、公司名称、保留所有权利的声明以及应用程序的版本。它们都水平居中并垂直扩展。 3. 在 MainPage.xaml.cs 文件中添加了一个名为 "OnNoticeClicked" 的新方法。此方法被设计为显示一个带有 "注意事项" 标题和包含关于照片存储路径、照片查看和信息过滤说明的消息的警告。警告包括一个 "OK" 按钮用于关闭。

--- BridgeInspectionApp/MainPage.xaml.cs
+++ BridgeInspectionApp/MainPage.xaml.cs
@@ -10,7 +10,14 @@ public partial class MainPage : ContentPage
         InitializeComponent();
     }
 
-
+    private async void OnNoticeClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("注意事项", 
+            "1、照片的存放路径是在内部存储设备中“桥梁巡查”目录，这个地方照片不能删除，不能随意改名字。\n"+
+            "2、单击照片可以放大查看。\n"+
+            "3、在搜索框中输入关键词可以过滤信息",
+            "OK");
+    }
     private async void OnViewInfoClicked(object sender, EventArgs e)
     {
         try
